a97b8e9957f22ce00b323f9ee139a3647f4a066e
Maicon Hellmann <maiconhellmann@gmail.com>
2017-01-10 08:24:55 -0200

    MainMenu and icon

--- app/src/main/java/br/com/forusers/heinscomponents/example/MainActivity.java
+++ app/src/main/java/br/com/forusers/heinscomponents/example/MainActivity.java
@@ -1,10 +1,18 @@
 package br.com.forusers.heinscomponents.example;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.bumptech.glide.Glide;
@@ -38,8 +46,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void onClickSetUri() {
-        glideImageView.setSkypMemoryCache(true);
-        glideImageView.setImageUri("http://www.w3schools.com/css/img_fjords.jpg");
+        try {
+            glideImageView.setSkypMemoryCache(true);
+            glideImageView.setImageUri("http://www.w3schools.com/css/img_fjords.jpg");
+        }catch (Exception e){
+            showErrorMessage(e);
+        }
     }
 
     private void onClickClearImage() {
@@ -66,5 +78,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             super.onPostExecute(aVoid);
             Glide.get(glideImageView.getImgView().getContext()).clearMemory();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_github:
+                onClickGithub();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void onClickGithub() {
+        try{
+            String url = getString(R.string.github_url);
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        } catch (Exception e) {
+            showErrorMessage(e);
+        }
+    }
+    private void showErrorMessage(Exception e){
+        Log.e(getClass().getSimpleName(), getString(R.string.error), e);
+
+        AlertDialog.Builder b = new AlertDialog.Builder(this);
+        b.setTitle(R.string.error);
+        b.setMessage(e.getMessage() != null ? e.getMessage() : e.getClass().getSimpleName());
+        b.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        b.show();
     }
 }
